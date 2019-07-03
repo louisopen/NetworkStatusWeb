d867b458d5ace3679692e8e301eea04f31830914
Louis Huang <louisopen@gmail.com>
2019-07-03 22:18:13 +0800

    update all

--- Network.py
+++ Network.py
@@ -26,8 +26,11 @@ def get_key():
     #print(net)
 
 def get_rate(func):     
+    import time
     key_info, old_recv, old_sent = func()  # 上一秒收集的数据
-    time.sleep(1) 
+ 
+    time.sleep(1)
+ 
     key_info, now_recv, now_sent = func()  # 当前所收集的数据
     net_in = {}
     net_out = {}
@@ -35,42 +38,42 @@ def get_rate(func):
     for key in key_info:
         net_in.setdefault(key, (now_recv.get(key) - old_recv.get(key)) / 1024 / 1024)  # 每秒接收速率MB
         net_out.setdefault(key, (now_sent.get(key) - old_sent.get(key)) / 1024 / 1024) # 每秒发送速率MB
-    
-    #old_recv = now_recv
-    #old_sent = now_sent
     return key_info, net_in, net_out
  
 
 
 
-while True:
-    try:
-        ip_file = open('networkstatus.txt','a')
-        ip_file.write(time.strftime("[%Y-%m-%d %H:%M:%S]")+'\n')
-  
+while 1:
+    from os.path import basename
+    from psutil import net_connections,Process
+
+    for x in net_connections('all'):
+        laddr, raddr, status, pid=x[3:]
+        if not raddr:
+            continue
+        try:
+            filename = basename(Process(pid).exe()) #專注於exe程序
+        except:
+            pass
+        else:
+            msg='''程序: {}  本地地址: {}  遠程地址: {}  連接狀態: {}'''.format(filename, laddr, raddr, status)
+            #print(msg)
+            ip_file = open('networkstatus.txt','a')
+            ip_file.write(time.strftime("%Y-%m-%d %H:%M:%S ") + msg + '\n')
+            ip_file.close()      
+    print(" ")
+    
+    #net = psutil.net_io_counters()
+    #print(net)
+    #net = psutil.net_connections()
+    #print(net)
+
+    try: 
         key_info, net_in, net_out = get_rate(get_key) 
         for key in key_info:           # 各網路介面(區域網路,bluebooth,WiFi,vEthernet)
-            msg='''Interface = {}\nReceived = {}MB/s\nTransmit = {}MB/s\n'''.format( key, net_in.get(key), net_out.get(key))
-            ip_file.write(msg)
-            #print("TIME = " + time.strftime("%Y-%m-%d %H:%M:%S "))
-            #print('%s\nInput:\t %-5sKB/s\nOutput:\t %-5sKB/s\n' % (key, net_in.get(key), net_out.get(key)))
+            print("TIME = " + time.strftime("%Y-%m-%d %H:%M:%S "))
+            print('%s\nInput:\t %-5sKB/s\nOutput:\t %-5sKB/s\n' % (key, net_in.get(key), net_out.get(key)))
          
-        from os.path import basename
-        from psutil import net_connections,Process
-        for x in net_connections('all'):
-            laddr, raddr, status, pid=x[3:]
-            if not raddr:
-                continue
-            try:
-                filename = basename(Process(pid).exe()) #專注於exe程序
-            except:
-                pass
-            else:
-                msg='''Process = {} Local = {} Remote = {} Connection = {}\n'''.format(filename, laddr, raddr, status)
-                #print(msg)
-                ip_file.write(msg)
-
-        ip_file.write('\n')
-        ip_file.close() 
+        
     except KeyboardInterrupt:
         exit()
